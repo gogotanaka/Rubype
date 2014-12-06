7d80e00cc7cddeb12e4a0c5f6f120571a522d80a
gogotanaka <mail@tanakakazuki.com>
2014-12-06 12:32:08 -0800

    Restructure files

--- lib/haskell.rb
+++ lib/haskell.rb
@@ -1,9 +1,4 @@
 require 'haskell/type_list'
-require 'haskell/base'
-require 'haskell/assert_arg_type'
-require 'haskell/assert_rtn_type'
-
-module Haskell; end
 
 # Builtin Contracts
 class  Any;     end
@@ -13,18 +8,40 @@ FalseClass.send(:include, Boolean)
 
 class Module
   private
-    def __haskell__
-      prepend (@__haskell__ = Module.new) unless @__haskell__
-      @__haskell__
+  def __haskell__
+    prepend (@__haskell__ = Module.new) unless @__haskell__
+    @__haskell__
+  end
+
+  def type(type_list, meth)
+    __haskell__.send(:define_method, meth) do |*args, &block|
+      ::Haskell.assert_arg_type(meth, args, type_list.args)
+      rtn = super(*args, &block)
+      ::Haskell.assert_trn_type(meth, rtn, type_list.rtn)
+      rtn
+    end
+    self
+  end
+end
+
+module Haskell
+  class << self
+    def assert_arg_type(meth, args, klasses)
+      args.each_with_index do |arg, i|
+        if wrong_type?(arg, klasses[i])
+          raise ArgumentError, "Wrong type of argument, type of #{arg.inspect} should be #{klasses[i]}"
+        end
+      end
     end
 
-    def type(type_list, meth)
-      __haskell__.send(:define_method, meth) do |*args, &block|
-        ::Haskell::AssertArgType.execute(meth, args, type_list.args)
-        rtn = super(*args, &block)
-        ::Haskell::AssertRtnType.execute(meth, rtn, type_list.rtn)
-        rtn
+    def assert_trn_type(meth, rtn, klass)
+      if wrong_type?(rtn, klass)
+        raise TypeError, "Expected #{meth} to return #{klass} but got #{rtn.inspect} instead"
       end
-      self
     end
+
+    def wrong_type?(obj, klass)
+      !(obj.is_a?(klass) || klass == Any)
+    end
+  end
 end
--- lib/haskell/base.rb
+++
@@ -1,7 +0,0 @@
-module Haskell
-  module Base
-    def wrong_type?(obj, klass)
-      !(obj.is_a?(klass) || klass == Any)
-    end
-  end
-end
