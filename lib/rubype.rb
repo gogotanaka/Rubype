c70ba98156662bda1bd4c1ae3990ffe6b0a19493
gogotanaka <mail@tanakakazuki.com>
2014-12-07 03:03:57 -0800

    Add documents

--- lib/rubype.rb
+++ lib/rubype.rb
@@ -1,5 +1,3 @@
-require "rubype/rubype"
-
 # Builtin Contracts
 class  Any;     end
 module Boolean; end
@@ -13,14 +11,15 @@ class Module
     @__rubype__
   end
 
+  # @param hash [Hash] {method_name: [ArgClass1, ArgClass2, ... ArgClassn => RtnClass]}
   def typesig(hash)
     meth = hash.keys.first
     *arg_types, type_pair = hash.values.first
 
     __rubype__.send(:define_method, meth) do |*args, &block|
-      ::Rubype.assert_arg_type(meth, args, arg_types << type_pair.keys.first)
+      ::Rubype.send(:assert_arg_type, meth, args, arg_types << type_pair.keys.first)
       rtn = super(*args, &block)
-      ::Rubype.assert_trn_type(meth, rtn, type_pair.values.first)
+      ::Rubype.send(:assert_trn_type, meth, rtn, type_pair.values.first)
       rtn
     end
     self
@@ -29,6 +28,11 @@ end
 
 module Rubype
   class << self
+    private
+
+    # @param meth [Symbol]
+    # @param args [Array<Object>]
+    # @param klasses [Array<Class>]
     def assert_arg_type(meth, args, klasses)
       args.each_with_index do |arg, i|
         if wrong_type?(arg, klasses[i])
@@ -37,12 +41,18 @@ module Rubype
       end
     end
 
+    # @param meth [Symbol]
+    # @param rtn [Object]
+    # @param klass [Class]
     def assert_trn_type(meth, rtn, klass)
       if wrong_type?(rtn, klass)
         raise TypeError, "Expected #{meth} to return #{klass} but got #{rtn.inspect} instead"
       end
     end
 
+    # @param obj [Object]
+    # @param klass [Class]
+    # @return [Boolean]
     def wrong_type?(obj, klass)
       !(obj.is_a?(klass) || klass == Any)
     end
