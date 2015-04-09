f8bae434cfc596c61f35ac5998d5c0cdb9a71292
Stefan Haflidason <styrmis@gmail.com>
2015-04-09 11:13:53 +0100

    Fixed minor grammatical issue with error messages

---
+++ lib/rubype/ordinal.rb
@@ -0,0 +1,15 @@
+# Borrowed from ActiveSupport::Inflector
+def ordinal(number)
+  abs_number = number.to_i.abs
+
+  if (11..13).include?(abs_number % 100)
+    "th"
+  else
+    case abs_number % 10
+      when 1; "st"
+      when 2; "nd"
+      when 3; "rd"
+      else    "th"
+    end
+  end
+end
