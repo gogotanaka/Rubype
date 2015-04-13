b794d727aceb815de50457e3bca55b995feed81b
Vsevolod <stefkin.v@gmail.com>
2015-04-13 23:29:50 +0300

    Fix warning
    MiniTest::Unit::TestCase is now Minitest::Test.

--- test/test_rubype.rb
+++ test/test_rubype.rb
@@ -4,7 +4,7 @@ class TypePair
     "#{last_arg_type} => #{rtn_type}"
   end
 end
-class TestRubype < MiniTest::Unit::TestCase
+class TestRubype < Minitest::Test
   def setup
     @string  = 'str'
     @numeric = 1
