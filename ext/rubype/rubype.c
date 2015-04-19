14b26d6ad7243be21432a85d72ae5b2fb5e9ecbe
Vsevolod <stefkin.v@gmail.com>
2015-04-19 19:00:40 +0300

    Use C implementation for Contract#assert_args_type
    and also Contract#assert_rtn_type

--- ext/rubype/rubype.c
+++ ext/rubype/rubype.c
@@ -1,9 +1,11 @@
 #include "rubype.h"
 
-VALUE rb_mRubype, rb_eRubypeArgumentTypeError, rb_eRubypeReturnTypeError, rb_eInvalidTypesigError;
+VALUE rb_mRubype, rb_cContract, rb_eRubypeArgumentTypeError, rb_eRubypeReturnTypeError, rb_eInvalidTypesigError;
+
 
 #define STR2SYM(x) ID2SYM(rb_intern(x))
-static ID id_is_a_p, id_to_s, id_plus;
+static ID id_is_a_p, id_to_s, id_plus, id_meth, id_owner, id_arg_types, id_rtn_type;
+
 #define f_boolcast(x) ((x) ? Qtrue : Qfalse)
 
 int unmatch_type_p(VALUE obj, VALUE type_info)
@@ -56,11 +58,18 @@ test(VALUE rubype, VALUE obj)
   return str;
 }
 static VALUE
-rb_rubype_assert_args_type(VALUE rubype, VALUE meth_caller, VALUE meth, VALUE args, VALUE type_infos, VALUE caller_trace)
+rb_rubype_assert_args_type(VALUE self, VALUE rubype, VALUE args)
 {
   int i;
-  VALUE arg, type_info, error_mes;
+  VALUE arg, type_info;
   VALUE target;
+  VALUE meth_caller, meth, type_infos;
+  meth_caller = rb_ivar_get(self, id_owner);
+  meth = rb_ivar_get(self, id_meth);
+  type_infos = rb_ivar_get(self, id_arg_types);
+
+  Check_Type(meth, T_SYMBOL);
+  Check_Type(type_infos, T_ARRAY);
 
   for (i=0; i<RARRAY_LEN(args); i++) {
     arg       = rb_ary_entry(args, i);
@@ -77,9 +86,16 @@ rb_rubype_assert_args_type(VALUE rubype, VALUE meth_caller, VALUE meth, VALUE ar
 }
 
 static VALUE
-rb_rubype_assert_rtn_type(VALUE rubype, VALUE meth_caller, VALUE meth, VALUE rtn, VALUE type_info, VALUE caller_trace)
+rb_rubype_assert_rtn_type(VALUE self, VALUE rubype, VALUE rtn)
 {
   VALUE target;
+  VALUE meth_caller, meth, type_info;
+  meth_caller = rb_ivar_get(self, id_owner);
+  meth = rb_ivar_get(self, id_meth);
+  type_info = rb_ivar_get(self, id_rtn_type);
+
+  Check_Type(meth, T_SYMBOL);
+
   if (unmatch_type_p(rtn, type_info)){
     target = rb_str_new2("");
     rb_str_catf(target, "%"PRIsVALUE"#%"PRIsVALUE"'s return", meth_caller, meth);
@@ -89,16 +105,31 @@ rb_rubype_assert_rtn_type(VALUE rubype, VALUE meth_caller, VALUE meth, VALUE rtn
 }
 
 void
+Init_contract(void)
+{
+  rb_cContract = rb_define_class_under(rb_mRubype, "Contract", rb_cObject);
+  rb_define_method(rb_cContract, "assert_args_type", rb_rubype_assert_args_type, 2);
+  rb_define_method(rb_cContract, "assert_rtn_type", rb_rubype_assert_rtn_type, 2);
+
+  id_meth = rb_intern("@meth");
+  id_owner = rb_intern("@owner");
+  id_arg_types = rb_intern("@arg_types");
+  id_rtn_type = rb_intern("@rtn_type");
+}
+
+void
 Init_rubype(void)
 {
   id_is_a_p = rb_intern_const("is_a?");
   id_to_s = rb_intern_const("to_s");
   id_plus = rb_intern_const("+");
   rb_mRubype  = rb_define_module("Rubype");
+
+
   rb_eRubypeArgumentTypeError = rb_define_class_under(rb_mRubype, "ArgumentTypeError", rb_eTypeError);
   rb_eRubypeReturnTypeError = rb_define_class_under(rb_mRubype, "ReturnTypeError", rb_eTypeError);
   rb_eInvalidTypesigError = rb_define_class_under(rb_mRubype, "InvalidTypesigError", rb_eTypeError);
-  rb_define_singleton_method(rb_mRubype, "assert_args_type", rb_rubype_assert_args_type, 5);
-  rb_define_singleton_method(rb_mRubype, "assert_rtn_type", rb_rubype_assert_rtn_type, 5);
   rb_define_singleton_method(rb_mRubype, "test", test, 1);
+
+  Init_contract();
 }
